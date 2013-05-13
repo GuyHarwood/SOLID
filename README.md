4aba5fe2e4837265b3c3c8dc06f06ac4f766da7d
MrBaldman <gpharwood@gmail.com>
2013-05-13 14:49:52 +0100

    tests and README.md

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 SOLID
 =====
 
-Good examples of SOLID implementation.  Respective authors mentioned in source
+Good examples of SOLID implementation.  
+
+OCP Example - http://lostechies.com/joeocampo/2008/03/21/ptom-the-open-closed-principle/
+
---
+++ SOLID/OcpTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace SOLID.ocp
+{
+    [TestFixture]
+    public class OcpTests
+    {
+        private ProductFilter filterProduct;
+        private IList<Product> products;
+
+        [SetUp]
+        public void before_each_spec()
+        {
+            this.filterProduct = new ProductFilter();
+            this.products = this.BuildProducts();
+        }
+
+        private IList<Product> BuildProducts()
+        {
+            return new List<Product>
+                               {
+                                   new Product(ProductColor.Blue),
+                                   new Product(ProductColor.Yellow),
+                                   new Product(ProductColor.Yellow),
+                                   new Product(ProductColor.Red),
+                                   new Product(ProductColor.Blue)
+                               };
+
+        }
+
+
+        [Test]
+        public void should_filter_by_the_color_given()
+        {
+            int foundCount = 0;
+            foreach (var product in this.filterProduct.By(this.products, new ColorFilterSpecification(ProductColor.Blue)))
+            {
+                foundCount++;
+            }
+
+            Assert.That(foundCount, Is.EqualTo(2));
+        }
+    }
+}
